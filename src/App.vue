d66bed9439c9594c7a6d2972122d1d877d0b97d8
Musa <dunedenmusa@gmail.com>
2024-05-05 15:57:22 -0400

    create dummy pages, split main page parts to components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
-<script setup>
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+import AsideComponent from '@/components/AsideComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import CarouselComponent from '@/components/CarouselComponent.vue'
+
+export default {
+  components: {
+    CarouselComponent,
+    FooterComponent,
+    AsideComponent,
+    HeaderComponent
+  }
+}
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline text-cyan-800">
-    Hello world!
-  </h1>
+  <header-component />
+  <div class="md:flex">
+    <main class="bg-blue-500 w-full md:basis-3/4">
+      <carousel-component />
+      <router-view />
+    </main>
+    <aside-component />
+  </div>
+  <footer-component />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/views/BeyondView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="grow">beyond</div>
+</template>
+
+<style scoped>
+
+</style>

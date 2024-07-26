cd886d3b419402588a48e89b442ec9d628e1d6af
Musa <dunedenmusa@gmail.com>
2024-07-25 22:16:34 -0400

    add lorem fillers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,8 @@
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-3">
-    <div class="bg-green-500 text-center">kitchen</div>
-    <div class="bg-fuchsia-500 text-center">flooring</div>
-    <div class="bg-gray-600 text-center">bath</div>
+    <div class="bg-green-500 text-center">Kitchen Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut iste magni mollitia pariatur sed tempora vel. Asperiores debitis explicabo libero minima nihil odio officiis porro praesentium ratione, saepe sequi, soluta?</div>
+    <div class="bg-fuchsia-500 text-center">Bath Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque hic quaerat rem vitae voluptatem. Assumenda esse repudiandae sit voluptate. Dolores eius incidunt iure minima nostrum perferendis quos recusandae reiciendis voluptatem.</div>
+    <div class="bg-gray-600 text-center">Flooring Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi deleniti dolores harum molestias numquam, quisquam tenetur? Accusantium excepturi fugit impedit non nostrum, quaerat rerum sed temporibus totam velit! Ab, nemo.</div>
   </div>
 </template>

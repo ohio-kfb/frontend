4396ba409ab6b8c4d7a6e6440759afb6f0663ca1
Musa <dunedenmusa@gmail.com>
2024-09-04 20:48:24 -0400

    i did this changes

--- src/components/AsideComponent.vue
+++ src/components/AsideComponent.vue
@@ -4,9 +4,11 @@
 
 <template>
   <aside class="w-full text-center md:basis-1/4 space-y-4">
-    <div>ad 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, quasi.</div>
-    <div>ad 2 Lorem ipsum dolor sit.</div>
-    <div>ad 3 Lorem ipsum dolor sit amet, consectetur.</div>
+    <div>
+      <p>contact us</p>
+      <p>telephone: 123456789</p>
+      <p>email: myeamil@mail.com</p>
+    </div>
   </aside>
 </template>
 

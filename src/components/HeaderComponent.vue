f1428fc55bb31131ba792cb10534548bce4adf94
Mykyta <mp091689@gmail.com>
2024-09-06 21:35:38 -0400

    fix header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,7 +5,7 @@ import SocialNetworkComponent from '@/components/SocialNetworkComponent.vue'
 </script>
 
 <template>
-  <header class="flex flex-wrap uppercase mb-4">
+  <header class="fixed left-0 top-0 w-full md:static z-50 flex flex-wrap uppercase mb-4">
     <div class="w-full items-center justify-between gap-x-4 px-4 md:flex hidden">
       <router-link :to="{name: 'home'}" class="py-2 hover:text-blue-500">
         <img class="w-48" src="/img/dummy-logo-3.png" alt="">

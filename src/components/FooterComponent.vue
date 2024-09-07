5fbe04ee0d3a42c92565b6c331dcc57c10256b8e
Mykyta <mp091689@gmail.com>
2024-09-07 16:48:12 -0400

    refactor home page, cards, links

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <footer class="flex w-full"><p class="w-full text-center">© 2024 UsZenith.</p></footer>
+  <footer class="flex w-full"><p class="w-full text-center">© 2024 UsZenith LLC.</p></footer>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,61 @@
 <script setup>
 import CardComponent from '@/components/CardComponent.vue'
-import HomePageCardContentComponent from '@/components/HomePageCardContentComponent.vue'
+import RLinkComponent from '@/components/RLinkComponent.vue'
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-3 gap-x-4">
-    <CardComponent foo="true">
-      <HomePageCardContentComponent>
-        <template #img>
-          <img src="@/assets/images/home/kit-2.jpg" alt="kitchen" class="object-center object-cover">
-        </template>
-        <template #title>
-          A Culinary Haven
-        </template>
-        <template #text>
-          Transform your kitchen into a culinary haven with our expert renovation services. From modern upgrades to
-          classic makeovers, we design and create functional, stylish spaces that meet your needs and reflect your
-          taste.
-        </template>
-      </HomePageCardContentComponent>
-    </CardComponent>
-    <CardComponent foo="true">
-      <HomePageCardContentComponent>
-        <template #img>
-          <img src="@/assets/images/home/bath-10.jpg" alt="bathroom" class="object-center object-cover">
-        </template>
-        <template #title>
-          Elevate your bathroom
-        </template>
-        <template #text>
-          Elevate your bathroom experience with our tailored renovation solutions. Whether you’re
-          dreaming of a spa-like retreat or a sleek, contemporary look, we craft beautiful, practical bathrooms that
-          offer
-          both comfort and elegance.
-        </template>
-      </HomePageCardContentComponent>
 
-    </CardComponent>
-    <CardComponent foo="true">
-      <HomePageCardContentComponent>
-        <template #img>
-          <img src="@/assets/images/home/floor-13.jpg" alt="flooring" class="object-center object-cover">
-        </template>
-        <template #title>
-          Premium flooring
-        </template>
-        <template #text>
-          Revitalize your home with our premium flooring options. From luxurious hardwood to
-          durable tile and cozy carpets, we provide expert installation and design advice to ensure your floors enhance
-          the
-          beauty and functionality of your space.
-        </template>
-      </HomePageCardContentComponent>
-    </CardComponent>
+    <r-link-component :to="{name: 'kitchen'}" class="no-underline border border-white hover:border-orange-600">
+      <CardComponent class="h-full">
+        <img src="@/assets/images/home/kit-2.jpg" alt="kitchen" class="h-44 w-full object-cover object-center">
+        <div>
+          <h3 class="font-bold text-lg mb-4">
+            A Culinary Haven
+          </h3>
+          <p>
+            Transform your kitchen into a culinary haven with our expert renovation services. From modern upgrades to
+            classic makeovers, we design and create functional, stylish spaces that meet your needs and reflect your
+            taste.
+          </p>
+        </div>
+      </CardComponent>
+    </r-link-component>
+
+    <r-link-component :to="{name: 'bath'}" class="no-underline border border-white hover:border-orange-600">
+      <CardComponent class="h-full">
+        <img src="@/assets/images/home/bath-10.jpg" alt="bathroom" class="h-44 w-full object-cover object-center">
+        <div>
+          <h3 class="font-bold text-lg mb-4">
+            Elevate your bathroom
+          </h3>
+          <p>
+            Elevate your bathroom experience with our tailored renovation solutions. Whether you’re
+            dreaming of a spa-like retreat or a sleek, contemporary look, we craft beautiful, practical bathrooms that
+            offer
+            both comfort and elegance.
+          </p>
+        </div>
+      </CardComponent>
+    </r-link-component>
+
+    <r-link-component :to="{name: 'flooring'}" class="no-underline border border-white hover:border-orange-600">
+      <CardComponent class="h-full">
+        <img src="@/assets/images/home/floor-13.jpg" alt="flooring"
+             class="h-36 md:h-44 w-full object-cover object-bottom">
+        <div>
+          <h3 class="font-bold text-lg mb-4">
+            Premium flooring
+          </h3>
+          <p>
+            Revitalize your home with our premium flooring options. From luxurious hardwood to
+            durable tile and cozy carpets, we provide expert installation and design advice to ensure your floors enhance
+            the
+            beauty and functionality of your space.
+          </p>
+        </div>
+      </CardComponent>
+    </r-link-component>
+
   </div>
 </template>
